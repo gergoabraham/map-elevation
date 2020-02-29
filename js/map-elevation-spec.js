54055a5c3091f35580ef601c1ce602f144a470e9
gergooo <39014407+gergooo@users.noreply.github.com>
2020-02-29 13:38:42 +0100

    elevateMap function implemented

--- js/map-elevation-spec.js
+++ js/map-elevation-spec.js
@@ -3,8 +3,154 @@ chai.should();
 
 ({elevateMap} = require('./map-elevation'));
 
-describe('init', function() {
-  it('should pass', function() {
-    elevateMap(3).should.equal(4);
+describe('MapElevation', function() {
+  const inputMap = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  it('should be a pure function', function() {
+    elevateMap(inputMap, 2, 6);
+    sumOfElements = inputMap.reduce(
+        (sum, row) => sum += row.reduce((rowSum, elem) => rowSum += elem), 0);
+
+    sumOfElements.should.equal(0);
+  });
+
+  context('simple elevation', function() {
+    it('should elevate at one point', function() {
+      elevateMap(inputMap, 2, 5).should.deep.equal([
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+      ]);
+    });
+  });
+
+  context('elevating neighbours', function() {
+    it('should elevate neighbours', function() {
+      const firstIteration = elevateMap(inputMap, 2, 5);
+      elevateMap(firstIteration, 2, 5).should.deep.equal([
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 2, 1],
+        [0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+      ]);
+    });
+
+    it('should elevate neighbours of neighbours', function() {
+      const firstIteration = elevateMap(inputMap, 2, 4);
+      const secondIteration = elevateMap(firstIteration, 2, 4);
+      const output = elevateMap(secondIteration, 2, 4);
+
+      output.should.deep.equal([
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 2, 1, 0],
+        [0, 0, 1, 2, 3, 2, 1],
+        [0, 0, 0, 1, 2, 1, 0],
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+      ]);
+    });
+
+    it('should elevate neighbours neighbours #2', function() {
+      const iteration1 = elevateMap(inputMap, 2, 4);
+      const iteration2 = elevateMap(iteration1, 2, 4);
+      const iteration3 = elevateMap(iteration2, 2, 4);
+      const iteration4 = elevateMap(iteration3, 3, 1);
+      const iteration5 = elevateMap(iteration4, 3, 1);
+      const iteration6 = elevateMap(iteration5, 3, 1);
+
+      iteration6.should.deep.equal([
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 1, 0, 1, 2, 1, 0],
+        [1, 2, 1, 2, 3, 2, 1],
+        [2, 3, 2, 1, 2, 1, 0],
+        [1, 2, 1, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+      ]);
+    });
+
+    it('should elevate neighbours neighbours #3', function() {
+      let iterations = elevateMap(inputMap, 3, 3);
+      for (let i = 0; i < 6; i++) {
+        iterations = elevateMap(iterations, 3, 3);
+      }
+
+      iterations.should.deep.equal([
+        [1, 2, 3, 4, 3, 2, 1],
+        [2, 3, 4, 5, 4, 3, 2],
+        [3, 4, 5, 6, 5, 4, 3],
+        [4, 5, 6, 7, 6, 5, 4],
+        [3, 4, 5, 6, 5, 4, 3],
+        [2, 3, 4, 5, 4, 3, 2],
+        [1, 2, 3, 4, 3, 2, 1],
+      ]);
+    });
+  });
+
+  context('boundaries', function() {
+    it('should skip upper neighbours', function() {
+      const firstIteration = elevateMap(inputMap, 0, 1);
+      elevateMap(firstIteration, 0, 1).should.deep.equal([
+        [1, 2, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+      ]);
+    });
+    it('should skip left neighbours', function() {
+      const firstIteration = elevateMap(inputMap, 2, 0);
+      elevateMap(firstIteration, 2, 0).should.deep.equal([
+        [0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+        [2, 1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+      ]);
+    });
+    it('should skip lower neighbour', function() {
+      const firstIteration = elevateMap(inputMap, 6, 3);
+      elevateMap(firstIteration, 6, 3).should.deep.equal([
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 2, 1, 0, 0],
+      ]);
+    });
+    it('should skip right neighbour', function() {
+      const firstIteration = elevateMap(inputMap, 2, 6);
+      elevateMap(firstIteration, 2, 6).should.deep.equal([
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 1, 2],
+        [0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+      ]);
+    });
   });
 });
--- js/map-elevation.js
+++ js/map-elevation.js
@@ -1,7 +1,52 @@
 
-function elevateMap(a) {
-  return ++a;
+function elevateMap(input, x, y) {
+  let area = cloneArray(input);
+
+  area[x][y]++;
+
+  const neighborCoordinates = generateNeighborCoordinates(x, y);
+
+  neighborCoordinates.forEach(([nx, ny]) => {
+    if (areCoordinatesOkay(nx, ny, area) &&
+        isDifferenceTooLarge(area, x, y, nx, ny)) {
+      area = elevateMap(area, nx, ny);
+    }
+  });
+
+  return area;
 }
+
+function generateNeighborCoordinates(x, y) {
+  return [
+    [x - 1, y],
+    [x + 1, y],
+    [x, y - 1],
+    [x, y + 1],
+  ];
+}
+
+function isDifferenceTooLarge(area, x, y, nx, ny) {
+  return area[x][y] - area[nx][ny] > 1;
+}
+
+function areCoordinatesOkay(nx, ny, area) {
+  return nx >= 0 &&
+    ny >= 0 &&
+    nx < area.length &&
+    ny < area[nx].length;
+}
+
+function cloneArray(input) {
+  const output = [];
+  for (let i = 0; i < input.length; i++) {
+    output[i] = [];
+    for (let j = 0; j < input[i].length; j++) {
+      output[i][j] = input[i][j];
+    }
+  }
+  return output;
+}
+
 
 if (module) {
   module.exports = {elevateMap};
