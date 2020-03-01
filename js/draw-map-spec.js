3abf3fbe9a43c3047d15abadfedca40188b73e2b
gergooo <39014407+gergooo@users.noreply.github.com>
2020-03-01 18:49:51 +0100

    NIce colors added for landscape

---
+++ js/draw-map-spec.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+chai.should();
+
+global.document = {body: {onload: {}}};
+({getColor, initColors} = require('./draw-map'));
+
+describe.only('Draw map spec', function() {
+  before(function() {
+    initColors();
+  });
+
+  it('should return colors', function() {
+    getColor(0).should.equal('#67ddf5');
+    getColor(1).should.equal('#fff08f');
+    getColor(3).should.equal('#81d952');
+  });
+
+  it('should interpolate missing colors', function() {
+    getColor(2).should.equal('#c0e571');
+    getColor(13).should.equal('#d8dbdd');
+  });
+});
--- js/draw-map.js
+++ js/draw-map.js
@@ -1,11 +1,25 @@
-const n = 50;
+const n = 50; // min 40
 let canvas;
 let ctx;
 let map;
 
+const minValue = -0.3 * n;
+const maxValue = 0.3 * n;
+const colors = {
+  [maxValue]: '#ffffff', // white snow
+  [0.2 * n]: '#9da4ab', // mountain blueish-greyish
+  [0.1 * n]: '#3b8017', // darker grass
+  '3': '#81d952', // grass
+  '1': '#fff08f', // sand
+  '0': '#67ddf5', // tropical blue
+  [-0.15 * n]: '#000f5e', // dark blue
+  [minValue]: '#000000', // black
+};
+
 function init() {
   ({canvas, ctx} = initCanvas());
   map = initMap();
+  initColors();
   drawMap(map);
 }
 
@@ -32,7 +46,7 @@ function onRightClick(e) {
 
 function onClick(e, updateMap) {
   const {x, y} = calculateCoordinates(e);
-  map = updateMap(map, x, y, [-10, 10]);
+  map = updateMap(map, x, y, [minValue, maxValue]);
   drawMap(map);
 }
 
@@ -73,9 +87,55 @@ function drawMap(map) {
 }
 
 function getColor(value) {
-  const grayscaleColor = value * 10 + 128;
-  const hexValue = grayscaleColor.toString(16);
-  return `#${hexValue}${hexValue}${hexValue}`;
+  return colors[value];
+}
+
+function initColors() {
+  let color0;
+  let index0;
+  let color1;
+  let index1 = minValue;
+
+  for (let i = minValue; i < maxValue + 1; i++) {
+    if (colors[i]) {
+      color0 = colors[i];
+      index0 = i;
+    } else {
+      if (index1 < i) {
+        ({color1, index1} = findNextColor(i, color1, index1));
+      }
+
+      const r = interpolateColor(color0, index0, color1, index1, i, 0);
+      const g = interpolateColor(color0, index0, color1, index1, i, 1);
+      const b = interpolateColor(color0, index0, color1, index1, i, 2);
+
+      colors[i] = `#${r}${g}${b}`;
+    }
+  }
+}
+
+function findNextColor(i, color1, index1) {
+  while (!colors[i]) {
+    i++;
+  }
+  color1 = colors[i];
+  index1 = i;
+  return {color1, index1};
+}
+
+function interpolateColor(lastColor, i0, nextColor, i1, i, hexIndex) {
+  const color0 = parseInt(lastColor.substr(2 * hexIndex + 1, 2), 16);
+  const color1 = parseInt(nextColor.substr(2 * hexIndex + 1, 2), 16);
+
+  const colorValue = (color1 - color0) / (i1 - i0) * (i - i0) + color0;
+  const colorHexValue = Math.round(Math.abs(colorValue)).toString(16);
+  const hexValueWithLeadingZero = ('0' + colorHexValue).substr(-2);
+
+  return hexValueWithLeadingZero;
 }
 
 document.body.onload = init;
+
+if (typeof module != 'undefined') {
+  module.exports = {getColor, initColors};
+}
